d6fd12ca1eebbaab73a2fd4071fd9ce5a2fa6d85
Saikonate <alan.aranapre@gmail.com>
2021-10-27 13:35:43 -0500

    productos y js

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,15 @@ module.exports = defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
+        index: resolve(__dirname, 'index.html'),
         ProductosProteinas: resolve(__dirname, 'Productos-Proteinas/index.html'),
-        ProductosPlayeras: resolve(__dirname, 'Productos-Playeras/index.html'),
-        ProductosShorts: resolve(__dirname, 'Productos-Shorts/index.html'),
+        ProductosSuplementos: resolve(__dirname, 'Productos-Suplementos/index.html'),
+        ProductosGanadores: resolve(__dirname, 'Productos-Ganadores/index.html'),
+        ProductosAminoacidos: resolve(__dirname, 'Productos-Aminoacidos/index.html'),
+        ProductosPreentreno: resolve(__dirname, 'Productos-Preentreno/index.html'),
+        ProductosPrecursores: resolve(__dirname, 'Productos-Precursores/index.html'),
+        ProductosTermogenicos: resolve(__dirname, 'Productos-Termogenicos/index.html'),
+        ProductosGlutaminas: resolve(__dirname, 'Productos-Glutaminas/index.html'),
         ProductosOtros: resolve(__dirname, 'Productos-Otros/index.html'),
         Promociones: resolve(__dirname, 'Promociones/index.html'),
         IMC: resolve(__dirname, 'IMC/index.html'),
